0fee2d30f6aa4154bd7bff9929d1a2e725893937
Yihui Xie <xie@yihui.name>
2015-04-21 22:41:38 -0500

    also consider opacity in legend

--- inst/htmlwidgets/leaflet.js
+++ inst/htmlwidgets/leaflet.js
@@ -579,6 +579,7 @@ var dataframe = (function() {
           if (data.legend.type === 'numeric') {
             gradSpan = $('<span/>').css({
               'background': 'linear-gradient(' + colors + ')',
+              'opacity': data.legend.opacity,
               'height': '100px',
               'width': '18px',
               'display': 'block'
@@ -604,8 +605,8 @@ var dataframe = (function() {
               labels.push(data.legend.na_label);
             }
             for (var i = 0; i < colors.length; i++) {
-              legendHTML += '<i style="background:' + colors[i] + '"></i> ' +
-                            labels[i] + '<br/>';
+              legendHTML += '<i style="background:' + colors[i] + ';opacity:' +
+                            data.legend.opacity + '"></i> ' + labels[i] + '<br/>';
             }
             div.innerHTML = legendHTML;
           }
